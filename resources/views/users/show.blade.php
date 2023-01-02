001c825105fe8dc79f41b7636d44e3e634edece0
LindomarCabral <lindomar.tecno@gmail.com>
2023-01-02 17:20:09 -0300

    delete users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,6 +8,12 @@
 <ul>
     <li>{{ $user->name }}</li>
     <li>{{ $user->email}}</li>
-    <li>{{ $user->created_at }}</li>
 </ul>
+
+<form action="{{ route('users.destroy', $user->id) }}" method="post">
+    @method('DELETE')
+    @csrf
+   <button type="submit">Deletar</button>
+</form>
+
 @endsection

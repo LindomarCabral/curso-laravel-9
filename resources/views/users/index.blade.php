7bfa5f5372ac82a79202d013c43d8a52ea12d5ba
LindomarCabral <lindomar.tecno@gmail.com>
2023-01-02 14:37:52 -0300

    validation

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,9 +11,11 @@
 
 <ul>
     @foreach ($users as $user)
+   <li>
     {{ $user->name}} -
     {{ $user->email}}
     | <a href="{{route('users.show', $user->id)}}">Detalhes</a>
+   </li>
     @endforeach
 </ul>
 @endsection
